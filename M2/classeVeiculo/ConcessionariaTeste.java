843a5ef52a23a151f6faa3a954ae40fe4e8da9f2
Agnes Midori <54656640+agnesmidori@users.noreply.github.com>
2020-03-13 01:22:26 -0300

    Update ConcessionariaTeste.java
    Testando os métodos  acelerar e da interface implementada DAO.

--- M2/classeVeiculo/ConcessionariaTeste.java
+++ M2/classeVeiculo/ConcessionariaTeste.java
@@ -1,4 +1,3 @@
-// one class needs to have a main() method
 public class ConcessionariaTeste
 {
   // arguments are passed using the text field below this editor
@@ -8,17 +7,39 @@ public class ConcessionariaTeste
       chevette.setFabricante("GM");
       chevette.setCambio("manual");
       chevette.setModelo("L");
-
+      chevette.setRegistro("blablabla");
+      chevette.acelerar();
+      
+      Carro corolla = new Carro(false);
+      corolla.setFabricante("Toyota");
+      corolla.setCambio("automatico");
+      corolla.setModelo("SL");
+      corolla.setRegistro("bambambam");
+      corolla.acelerar();
 
       Onibus sogal = new Onibus();
       sogal.setFabricante("Scania");
       sogal.setModelo("Veloce");
+      sogal.setRegistro("blebleble");
+      sogal.acelerar();
 
 
       Concessionaria loja = new Concessionaria();
       loja.salvar(chevette);
       loja.salvar(sogal);
-
+      loja.print();
+      System.out.println("--------------");
+      
+      loja.atualizar(corolla, "blebleble");
+      loja.print();
+      System.out.println("--------------");
+      
+      loja.deletar("blablabla");
+      loja.deletar("blebleble");
+      loja.print();      
+      System.out.println("--------------");
+      
+      loja.atualizar(chevette, "bambambam");
       loja.print();
 
   }
